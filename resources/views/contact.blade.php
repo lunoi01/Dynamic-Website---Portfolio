631a05fdf11304410bd9dc8808ff2dd6a67fc446
Puteri Munajjah <pmunajjah@gmail.com>
2022-11-29 16:37:03 +0800

    Controller and URL Request Part 1

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact page</title>
+</head>
+<body>
+    <h2> This is a contact page from controller </h2>
+</body>
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,22 @@
+
+
+<?php
+    $world = "Hi Ariyan";
+    echo $world;
+?>
+
+{{ $world }} //bladesyntax
+
+if($world) {
+
+} else {
+
+}
+
+@if() //bladesyntax
+
+@endif //bladesyntax
+
+@foreach
+
+@endforeach
