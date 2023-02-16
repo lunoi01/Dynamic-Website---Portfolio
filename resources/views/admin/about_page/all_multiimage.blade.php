7a1e3df0b9fd17b16792b0e0b5acf0e75ced33db
Luna <80620073+lunoi01@users.noreply.github.com>
2023-02-16 10:18:29 +0800

    Backend Add Multi Image in About Page Part 4

--- resources/views/admin/about_page/all_multiimage.blade.php
+++ resources/views/admin/about_page/all_multiimage.blade.php
@@ -41,7 +41,7 @@
                         style="width: 60px; height: 60px;"></td>
 
                         <td>
-                            <a href="" class="btn btn-info sm" title="Edit Data"> <i class="fas fa-edit"></i> </a>
+                            <a href="{{ route('edit.multi.image',$item->id) }}" class="btn btn-info sm" title="Edit Data"> <i class="fas fa-edit"></i> </a>
  
                             <a href="" class="btn btn-danger sm" title="Delete Data"> <i class="fas fa-trash-alt"></i> </a>
                         </td>
