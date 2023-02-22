cfb27f2b64a9e84f01f362fa900fc0f6bc3e19ac
Luna <80620073+lunoi01@users.noreply.github.com>
2023-02-22 14:14:36 +0800

    Form Validation With JavaScript Part 1

--- app/Http/Controllers/Home/BlogCategoryController.php
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -24,16 +24,6 @@ class BlogCategoryController extends Controller
 
     public function StoreBlogCategory(Request $request){
 
-         $request->validate([
-            'blog_category' => 'required' 
-
-        ],[
-
-            'blog_category.required' => 'Blog Cateogry Name is Required',
-
-        ]);
-
-
             BlogCategory::insert([
                 'blog_category' => $request->blog_category, 
                 'created_at' => Carbon::now(),              
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -146,6 +146,8 @@
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
         <script src="{{ asset('backend/assets/js/code.js') }}"></script>
 
+        <script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
+
         <!-- Tags js -->
         <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js" ></script>  
 
